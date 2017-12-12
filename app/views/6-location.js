73650eec8ff3879a71bf5f6bd444f14432b6022d
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-12-12 19:36:16 +0000

    :nut_and_bolt: headings, content

--- app/views/6-location.js
+++ app/views/6-location.js
@@ -1,10 +1,18 @@
 module.exports = function (input, req) {
   if (input.loc !== undefined) {
     req.session.validated = Object.assign({}, req.session.validated, {
-      loc: input.loc,
+      loc: input.loc
     })
-
     input.redirect = '/7-results'
+  }
+
+  input.multiChoose = req.session.validated.multiChoose;
+  if ((input.multiChoose.includes('choose-location')) && (input.multiChoose.includes('choose-pharmacy'))) {
+    input.locationHeading = 'Where do you want to get a test?'
+  } else if (input.multiChoose.includes('choose-location')) {
+    input.locationHeading = 'Where do you want to find a sexual health professional?'
+  } else {
+    input.locationHeading = 'Where do you want to get a self-test kit from?'
   }
 
   return input
--- app/views/7-results.js
+++ app/views/7-results.js
@@ -6,7 +6,7 @@ module.exports = function (input, req) {
   if (input.hasSymptoms === 'yes') {
     if (input.multiChoose.includes('choose-location')) {
       input.locationHeading = 'Places you can see a sexual heath professional'
-      input.partialHeading = [];
+      input.partialHeading = [undefined];
       input.partials = ['sexual-health-providers'];
     }
   } else if (input.hasSymptoms === 'no') {
@@ -14,57 +14,45 @@ module.exports = function (input, req) {
       if ((input.multiChoose.includes('choose-location')) && (input.multiChoose.includes('choose-pharmacy'))
         && (input.multiChoose.includes('choose-online'))) {
         if (parseInt(input.age) >= 25) {
-          input.locationHeading = 'Where you can get a test'
           input.partialHeading = ['sexual-health-providers', 'pharmacies', 'online-providers'];
-          input.partials = input.partialHeading;
         } else if ((parseInt(input.age) >= 16) && (parseInt(input.age) < 25)) {
-          input.locationHeading = 'Where you can get a test'
           input.partialHeading = ['pharmacies', 'online-providers', 'sexual-health-providers'];
-          input.partials = input.partialHeading;
         }
+        input.locationHeading = 'Where you can get a test';
+        input.partials = input.partialHeading;
       }
     } else if (input.multiChoose.length == 2) {
       if ((input.multiChoose.includes('choose-location')) && (input.multiChoose.includes('choose-pharmacy'))) {
         if (parseInt(input.age) >= 25) {
-          input.locationHeading = 'Where you can get a test'
           input.partialHeading = ['sexual-health-providers', 'online-providers'];
-          input.partials = ['sexual-health-providers', 'online-providers'];
         } else if ((parseInt(input.age) >= 16) && (parseInt(input.age) < 25)) {
-          input.locationHeading = 'Where you can get a test'
           input.partialHeading = ['pharmacies', 'sexual-health-providers'];
-          input.partials = input.partialHeading;
         }
       }
       if ((input.multiChoose.includes('choose-online')) && (input.multiChoose.includes('choose-location'))) {
         if (parseInt(input.age) >= 25) {
-          input.locationHeading = 'Where you can get a test'
           input.partialHeading = ['sexual-health-providers', 'online-providers'];
-          input.partials = input.partialHeading;
         } else if ((parseInt(input.age) >= 16) && (parseInt(input.age) < 25)) {
-          input.locationHeading = 'Where you can get a test'
           input.partialHeading = ['online-providers', 'sexual-health-providers'];
-          input.partials = input.partialHeading;
         }
       }
       if ((input.multiChoose.includes('choose-online')) && (input.multiChoose.includes('choose-pharmacy'))) {
-        input.locationHeading = 'Where you can get a test'
         input.partialHeading = ['pharmacies', 'online-providers'];
-        input.partials = ['pharmacies', 'online-providers'];
       }
+      input.locationHeading = 'Where you can get a test';
+      input.partials = input.partialHeading;
     } else {
       if (input.multiChoose.includes('choose-location')) {
         input.locationHeading = 'Places you can see a sexual heath professional'
-        input.partialHeading = [];
         input.partials = ['sexual-health-providers'];
       } else if (input.multiChoose.includes('choose-pharmacy')) {
         input.locationHeading = 'Pharmacies where you can buy a self-test kit'
-        input.partialHeading = [];
         input.partials = ['pharmacies'];
       } else if (input.multiChoose.includes('choose-online')) {
         input.locationHeading = 'Where to order a self-test kit online'
-        input.partialHeading = [];
         input.partials = ['online-providers'];
       }
+      input.partialHeading = [undefined];
     }
   }
 
